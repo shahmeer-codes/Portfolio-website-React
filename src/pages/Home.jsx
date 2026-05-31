317f5e0b163ec956c2a0b43ea0e023446764e6d8
shahmeer <ranashahmeerarshad@gmail.com>
2026-05-31 05:31:46 -0700

    Fix links of home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,8 @@ import profilePic from "../assets/pics/profile_pic/2c.webp";
 const Home = () => {
   return (
     <section id="home">
-      <div className="min-h-screen bg-black text-white flex flex-col-reverse md:flex-row justify-around items-center px-4 md:px-10 py-20 md:py-0 gap-10 md:gap-0">
-        <div className="max-w-4xl text-center md:text-left flex flex-col items-center md:items-start">
+      <div className="min-h-screen bg-black text-white flex justify-around items-center px-4 md:px-10">
+        <div className="max-w-4xl">
           <p className="text-gray-400 text-xl mb-3">Hello, I'm</p>
 
           <h1 className="text-5xl md:text-7xl font-bold mb-4">
@@ -16,7 +16,6 @@ const Home = () => {
           </h1>
 
           <div className="text-2xl md:text-4xl font-semibold mb-6">
-            <span className="text-gray-300">I'm a </span>
             <TypeAnimation
               sequence={[
                 "MERN Stack Developer",
@@ -25,11 +24,11 @@ const Home = () => {
                 3000,
                 "Web Developer",
                 3000,
-                "Next.js Developer",
+                "Next.js Basics",
                 3000,
-                "React & Redux Developer",
+                "React & Redux ",
                 3000,
-                "Node.js & Express.js Developer",
+                "Node.js & Express.js",
                 3000,
                 "Frontend Developer",
                 3000,
@@ -37,7 +36,7 @@ const Home = () => {
                 3000,
                 "TypeScript & JavaScript Developer",
                 3000,
-                "Git & GitHub User",
+                "Git & GitHub ",
                 3000,
                 "Mongodb & Mysql Developer",
                 3000,
@@ -57,7 +56,7 @@ const Home = () => {
             solutions that provide exceptional user experiences and solve
             real-world problems.
           </p>
-          <div className="flex flex-wrap justify-center md:justify-start gap-4 mt-8">
+          <div className="flex gap-4 mt-6">
             <a
               href="#contact"
               className="px-5 py-3 border flex gap-2 rounded-full items-center justify-center border-green-500 text-green-500 hover:bg-green-500 hover:text-white transition"
@@ -73,7 +72,7 @@ const Home = () => {
               Download Resume
             </a>
           </div>
-          <div className="flex justify-center md:justify-start gap-6 mt-8 md:mt-5 md:ml-2">
+          <div className="flex gap-6 mt-5 ml-2">
             <a
               href="https://github.com/shahmeer-codes"
               target="_blank"
@@ -84,7 +83,7 @@ const Home = () => {
             </a>
 
             <a
-              href="https://www.linkedin.com/in/shahmeer-arshad-9b1a4b1b7/"
+              href="https://www.linkedin.com/in/shahmeer-arshad-07b3123b4/"
               target="_blank"
               rel="noopener noreferrer"
               className="text-gray-400 hover:text-blue-500 hover:scale-110 transition-all duration-300"
@@ -108,7 +107,7 @@ const Home = () => {
             alt="Your Name"
             width={400}
             height={400}
-            className="w-64 h-64 md:w-100 md:h-100 object-cover rounded-full border-4 border-green-400 mb-8 md:mb-0"
+            className="w-100 h-100 object-cover rounded-full border-4 border-green-400"
           />
         </div>
       </div>
